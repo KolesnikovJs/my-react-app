75f719f1141f4c5fe43b64200ec90e6c00760854
Vitalik Kolesnikov <kolesnikov.frontend@gmail.com>
2023-12-05 22:51:14 +0300

    commit test2

--- src/pages/Posts/Post.jsx
+++ src/pages/Posts/Post.jsx
@@ -1,12 +1,29 @@
+
+import { useEffect, useState } from 'react';
 import './Post.scss'
-const Post = ( {categories, subcategory, brand, items, cashback } ) => {
+// import productsList from '../Products/ProductsList';
+
+const Post = ( { productsChecked,setProductChecked, index, categories, subcategory, brand, items, cashback } ) => {
+
+    const [isChecked, setIsChecked]= useState(false)
+
+    useEffect(() => {
+        if(isChecked) {
+            productsChecked.push(index);
+            setProductChecked(productsChecked);
+            console.log(productsChecked);
+        }
+    }, [isChecked, index, productsChecked])
+   
+
+
     return (
         <div className='posts'>
-            <input className='input__post' type='checkbox'/>
+            <input className='input__post' type='checkbox' onChange={()=>setIsChecked(!isChecked)}/>
             <span>{categories}</span>
             <span>{subcategory}</span>
-            <span>{brand}</span>
-            <span>{items}</span>
+            <span>{brand!== null ? brand : "-"}</span>
+            <span>{items!== null ? items : "-"}</span>
             <span>{cashback}</span>
         </div>
     )
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,32 +1,32 @@
+/* eslint-disable no-unused-vars */
 import Button from "../Button/Button";
 import MenuItems from "../MenuItems/MenuItems";
 import Search from "../Search/Search";
 import Post from "../Posts/Post";
 import ModalDelete from "../ModalDelete/ModalDelete"
+import  productsList  from "../Products/ProductsList";
 import "./Products.scss"
-// import { useState } from "react";
+ import { useEffect, useState } from "react";
+
 
 const Products = () => {
-        
+     const [productState, setProductState]= useState(productsList);
+     const [productsChecked, setProductsChecked] = useState([]);
+     useEffect(()=>{
+        console.log(productState.length);
+    },[productState])
+   
+console.log(productsChecked);
     return (
         <>
             <Search/>
             <Button/>
             <MenuItems/>
             <div className="posts__products">
-            <Post categories="Эстетический уход" subcategory="Очищение" brand="-" items="-" cashback="20%"/>
-            <Post categories="Эстетический уход" subcategory="Скрабы" brand="Academie" items="Гоммаж с кремом и витамином У, 50мл" cashback="10%"/>
-            <Post categories="Эстетический уход" subcategory="Тонизация" brand="Academie" items="Нормализующий лосьон, 200мл" cashback="10%"/>
-            <Post categories="Эстетический уход" subcategory="Кремы и гели" brand="Academie" items="Восстанавливающий уход с кальцием, 50мл" cashback="15%"/>
-            <Post categories="Эстетический уход" subcategory="Очищение" brand="-" items="-" cashback="20%"/>
-            <Post categories="Эстетический уход" subcategory="Очищение" brand="-" items="-" cashback="20%"/>
-            <Post categories="Эстетический уход" subcategory="Очищение" brand="-" items="-" cashback="20%"/>
-            <Post categories="Эстетический уход" subcategory="Очищение" brand="-" items="-" cashback="20%"/>
-            <Post categories="Эстетический уход" subcategory="Очищение" brand="-" items="-" cashback="20%"/>
-            <Post categories="Эстетический уход" subcategory="Очищение" brand="-" items="-" cashback="20%"/>
-            <Post categories="Эстетический уход" subcategory="Очищение" brand="-" items="-" cashback="20%"/>
+            {productsList.map((item, index) => <Post key={index} index={index} productsChecked={productsChecked} setProductChecked={setProductsChecked} id={item.id} categories={item.categories} subcategory={item.subcategory} brand={item.brand} items={item.items} cashback={item.cashback}/>)}
             </div>
-            <ModalDelete/>
+            {/* {productsChecked.length > 0 ? <ModalDelete/> : ''} */}
+            <ModalDelete productsChecked={productsChecked} />
         </>
     )
 }
